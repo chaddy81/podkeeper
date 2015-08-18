77affdf016534c9e2e825902d521c0bd902c9258
Chad Bartels <chad.bartels@gmail.com>
2015-08-17 22:46:24 -0400

    [Finishes #101450858] Added join a pod paragraph

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,25 +5,25 @@ class RegistrationsController < ApplicationController
 
   def new
     if params[:auth_token]
-      invite = Invite.find_by_auth_token(params[:auth_token])
+      @invite = Invite.find_by_auth_token(params[:auth_token])
 
       if signed_in?
-        if current_user? invite.invitee
+        if current_user? @invite.invitee
           redirect_to root_path and return
         else
           sign_out
-          if invite.invitee.present?
+          if @invite.invitee.present?
             redirect_to invites_path
           else
             redirect_to signup_path(auth_token: params[:auth_token]) and return
           end
         end
       else
-        @pod = invite.pod
-        @user = User.new(email: invite.email,
-                         first_name: invite.first_name,
-                         last_name: invite.last_name,
-                         invite_id: invite.id)
+        @pod = @invite.pod
+        @user = User.new(email: @invite.email,
+                         first_name: @invite.first_name,
+                         last_name: @invite.last_name,
+                         invite_id: @invite.id)
       end
     else
       @user = User.new
