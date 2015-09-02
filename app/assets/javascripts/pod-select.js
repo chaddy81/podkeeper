2c319c471d3d7764da4441862845bb4744a101d0
Chad Bartels <chad.bartels@gmail.com>
2015-09-02 19:04:59 -0400

    Made pod overlay scroll on mobile

--- app/assets/javascripts/pod-select.js
+++ app/assets/javascripts/pod-select.js
@@ -1,6 +1,11 @@
 $(function() {
   $('.pod-nav__pod-name').on('click', function() {
     $('.pod-select__overlay').toggleClass('active');
+    if($('.pod-select__overlay.active').length) {
+      $('body').addClass('no-scroll');
+    }else{
+      $('body').removeClass('no-scroll');
+    }
   });
 
   $('.pod-select__pod').on('click', function() {
