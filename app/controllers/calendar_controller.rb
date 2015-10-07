2365a7780447d4cacf82b433d4f235df964473f9
Chad Bartels <chad.bartels@gmail.com>
2015-10-07 14:32:49 -0400

    Fix comment mailer

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -3,7 +3,7 @@ class CalendarController < ApplicationController
     if params[:start_date]
       @events = current_user_events.select { |ev| ev.start_date.month == params[:start_date].to_date.month && ev.start_date.year == params[:start_date].to_date.year }
     else
-      @events = current_user_events.select { |ev| ev.start_date.month == Date.today.month && ev.start_date.year == Date.today.year }
+      @events = current_user_events_upcoming.select { |ev| ev.start_date.month == Date.today.month && ev.start_date.year == Date.today.year }
     end
   end
 
@@ -23,6 +23,18 @@ class CalendarController < ApplicationController
 
     current_user.pod_memberships.includes(:pod).each do |pm|
       pm.pod.events.confirmed.order(:start_date, :start_time).each do |ev|
+        events << ev
+      end
+    end
+
+    return events
+  end
+
+  def current_user_events_upcoming
+    events = []
+
+    current_user.pod_memberships.includes(:pod).each do |pm|
+      pm.pod.events.confirmed.upcoming.order(:start_date, :start_time).each do |ev|
         events << ev
       end
     end
