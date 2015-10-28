36b2637cea1931863e7d3bcc73e40b0c906cb532
Chad Bartels <chad.bartels@gmail.com>
2015-10-28 19:17:43 -0400

    Added addtohomescreen script and added settings

--- app/assets/javascripts/home-page.js
+++ app/assets/javascripts/home-page.js
@@ -8,4 +8,11 @@ $(function() {
       scrollTop: $($target).offset().top + 'px'
     }, 500);
   });
+
+  if(moment().day() === 1 || moment().day() === 3) {
+    addToHomescreen({
+      appID: 'podkeeper',
+      maxDisplayCount: 0
+    });
+  }
 });
