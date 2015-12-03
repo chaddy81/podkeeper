0a58cccf066d1fdf15cfd48805e264795b98ed67
Chad Bartels <chad.bartels@gmail.com>
2015-12-03 13:33:40 -0500

    Working on performance fixes

--- config/environments/testing.rb
+++ config/environments/testing.rb
@@ -15,6 +15,10 @@ Rails.application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
+  # Compress JavaScripts and CSS.
+  config.assets.js_compressor = :uglifier
+  # config.assets.css_compressor = :sass
+
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
